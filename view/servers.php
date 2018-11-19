09c05c6177eda9044f3ecf23de411c7323b1bf56
root <guillaume.cyril.zisa@gmail.com>
2018-11-19 20:40:40 +0100

    Update bootstrap

--- view/servers.php
+++ view/servers.php
@@ -7,21 +7,94 @@
        </div></center>
        <section class="jumbotron ">
          <div class="ml-2">
-         <strong><h4>Installation d'un serveur web:</h4></strong>
-         Installation de la suite lamp ( Linux , Apache2 , Mariadb , Php ).<br>
-          <a class="btn btn-dark" href="../controller/redirection.php?action=server_web">Serveur Web</a><br><br>
-          <strong><h4>Installation dns:</h4></strong>
-          Installation dns ( Bind9 , Dnsutils ) .<br>
-           <a class="btn btn-dark" href="../controller/redirection.php?action=server_dns">Serveur Dns</a><br><br>
-           <strong><h4>Installation d'un serveur mail:</h4></strong>
-           Installation d'un serveur de mail ( PostFix ).<br>
-           <a class="btn btn-dark" href="../controller/redirection.php?action=server_mail">Serveur Mail</a><br><br>
-           <strong><h4>Gestion des utilisateurs:</h4></strong>
-            Ajouter & supprimer des utilisateurs, modification de nom ou de mot de passe d'utilisateurs .<br>
-            <a class="btn btn-dark" href="../controller/redirection.php?action=user">Gestion des utilisateurs</a><br><br>
-           <strong><h4>Gestion des groupes:</h4></strong>
-           Installation du service sudo, création et modification de groupes d'utilisateurs.<br> Gestion de privileges.<br>
-           <a class="btn btn-dark" href="../controller/redirection.php?action=group">Gestion des groupes</a><br><br>
+           <div class="container">
+             <div class="row">
+               <div class="col-sm-3">
+                 <div class="card text-center text-white bg-warning">
+                   <div class="card-header">
+                     <strong><h4>Installation d'un serveur web</h4></strong>
+                   </div>
+                   <div class="card-body ">
+                     <h5 class="card-title ">Installation de lamp <br>Apache2, Mariadb <br>& Php.
+                     </h5>
+
+                   </div>
+                   <div class="card-footer text-muted">
+                     <a class="btn btn-dark" href="../controller/redirection.php?action=server_web">Serveur Web</a><br>
+                   </div>
+                 </div>
+               </div>
+               <div class="col-sm-3">
+                 <div class="card text-center text-white bg-danger">
+                   <div class="card-header">
+                     <strong><h4>Installation d'un serveur dns</h4></strong>
+                   </div>
+                   <div class="card-body">
+                     <h5 class="card-title">Installation d'un dns <br>( Bind9 , Dnsutils ) .<br><br></h5>
+                   </div>
+                   <div class="card-footer text-muted">
+                     <a class="btn btn-dark" href="../controller/redirection.php?action=server_dns">Serveur Dns</a>
+                   </div>
+                 </div>
+               </div>
+               <div class="col-sm-3">
+                 <div class="card text-center text-white bg-primary">
+                   <div class="card-header">
+                     <strong><h4>Installation d'un serveur mail</h4></strong>
+                   </div>
+                   <div class="card-body">
+                     <h5 class="card-title">Installation d'un serveur de mail ( PostFix ).<br><br></h5>
+                   </div>
+                   <div class="card-footer text-muted">
+                     <a class="btn btn-dark" href="../controller/redirection.php?action=server_mail">Serveur Mail</a>
+                   </div>
+                 </div>
+               </div>
+               <div class="col-sm-3">
+                 <div class="card text-center text-white bg-success">
+                   <div class="card-header">
+                     <strong><h4>Configuration IpFixe:</h4></strong>
+                   </div>
+                   <div class="card-body">
+                     <h5 class="card-title">Configuration d'une ip fixe et de la résolution de noms.<br></h5>
+                   </div>
+                   <div class="card-footer text-muted">
+                     <a class="btn btn-dark" href="../controller/redirection.php?action=server_ip">IpFixe</a>
+                   </div>
+                 </div>
+               </div><br>
+               <div class="container">
+                 <div class="row">
+                   <div class="col-sm-6">
+                     <div class="card text-center text-white bg-dark">
+                       <div class="card-header">
+                         <strong><h4>Gestion des utilisateurs:</h4></strong>
+                       </div>
+                       <div class="card-body">
+                         <h5 class="card-title">Ajouter & supprimer des utilisateurs, modification de nom ou de mot de passe d'utilisateurs .</h5>
+                       </div>
+                       <div class="card-footer text-muted">
+                         <a class="btn btn-dark" href="../controller/redirection.php?action=user">Gestion des utilisateurs</a>
+                       </div>
+                     </div>
+                   </div>
+                   <div class="col-sm-6">
+                     <div class="card text-center text-white bg-secondary">
+                       <div class="card-header">
+                         <strong><h4>Gestion des groupes:</h4></strong>
+                       </div>
+                       <div class="card-body">
+                         <h5 class="card-title">Installation du service sudo, création et modification de groupes d'utilisateurs & gestion de privileges.</h5>
+                       </div>
+                       <div class="card-footer text-muted">
+                         <a class="btn btn-dark" href="../controller/redirection.php?action=group">Gestion des groupes</a>
+                       </div>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
          </div>
        </section>
    </main>
