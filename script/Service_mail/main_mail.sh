e6045dd0f90b079b8b6b8efcdb7752b0c203a331
SoluX1 <33317508+SoluX1@users.noreply.github.com>
2018-11-21 11:31:12 +0100

    Fin 1ère version du script d'install mail

--- script/Service_mail/main_mail.sh
+++ script/Service_mail/main_mail.sh
@@ -16,7 +16,7 @@ debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Si
 verify
 apt-get install -y postfix
 verify
-apt-get install -y dovecot-imapd
+apt-get install -y dovecot-core dovecot-imapd dovecot-mysql
 verify
 apt-get install -y mariadb-server
 verify
@@ -30,24 +30,17 @@ verify
 echo "---------- Fin de l'installation ---------"
 sleep 1
 # Début de configuration de POSTFIX
-echo ""
 # Installation d'une base de donnée MYSQL pour gérer les utilisateurs
-
 echo "---------- Début configuration MYSQL ----------"
-echo "";
 
 # Récupération des données du nouvel utilisateur
 
 echo "----- Création d'un administrateur MYSQL -----"
-echo ""
 
 # Remplacer par les variables des forms PHP
 read -p "Définir un Utilisateur : " user
-echo ""
 read -p "Définir un mot de passe MYSQL : " mdp
-echo ""
 read -p "Le nom de vôtre domaine : " domain
-
 
 # Création du nouvel utilisateur et attribution des droits
 sudo mysql -u root  -e "CREATE USER '$user' IDENTIFIED BY '$mdp' ;"
@@ -103,32 +96,50 @@ INSERT INTO domains VALUES ('', '$domain');
 INSERT INTO users VALUES ('', 1, PASSWORD('$mdp'), '$user@$domain', '$domain/$user/');
 EOF
 
-echo ""
 echo "---------- Fin de configuration MYSQL ----------"
 sleep 1
-echo ""
 echo "---------- Début de configuration arborescence --------"
-echo ""
 
 chmod 666 /etc/postfix/main.cf 
 
 # Ajout des lignes dans main.cf
 
-echo -e "# Ajout configuration\nvirtual_mailbox_domains = mysql:/etc/posfix/mysql-virtual-mailbox-domains.cf\nvirtual_mailbox_base = /var/mail/vhosts\nvirtual_mailbox_maps = mysql:/etc/postfix/mysql-virtual-mailbox-domains.cf\nvirtual_minimum_uid = 100\nvirtual_uid_maps = static:5000\nvirtual_gid_maps = static:5000\n" >> /etc/postfix/main.cf
+echo -e "# Ajout configuration\nvirtual_mailbox_domains = mysql:/etc/postfix/mysql-virtual-mailbox-domains.cf\nvirtual_mailbox_base = /var/mail/vhosts\nvirtual_mailbox_maps = mysql:/etc/postfix/mysql-virtual-mailbox-domains.cf\nvirtual_minimum_uid = 100\nvirtual_uid_maps = static:5000\nvirtual_gid_maps = static:5000\n" >> /etc/postfix/main.cf
 
-mkdir /etc/postfix/mysql-virtual-mailbox-domains.cf
-echo -e "user = $user \npassword = $mdp \nhosts = 127.0.0.1 \n dbname = messagerie \n query = SELECT 1 FROM virtual_domains WHERE name='%s'" >> /etc/postfix/mysql-virtual-mailbox-domains.cf
+touch /etc/postfix/mysql-virtual-mailbox-domains.cf
+echo -e "user = $user \npassword = $mdp \nhosts = 127.0.0.1 \ndbname = messagerie \nquery = SELECT 1 FROM domains WHERE name='%s'" >> /etc/postfix/mysql-virtual-mailbox-domains.cf
 systemctl restart postfix
 postmap -q $domain mysql:/etc/postfix/mysql-virtual-mailbox-domains.cf
 
-mkdir /etc/postfix/mysql-virtual-mailbox-maps.cf
-echo -e "user = $user \npassword = $mdp \nhosts = 127.0.0.1 \ndbname = messagerie \nquery = SELECT  maildir FROM virtual_users WHERE email='%s'" >> /etc/postfix/mysql-virtual-mailbox-maps.cf
+touch /etc/postfix/mysql-virtual-mailbox-maps.cf
+echo -e "user = $user \npassword = $mdp \nhosts = 127.0.0.1 \ndbname = messagerie \nquery = SELECT  maildir FROM users WHERE email='%s'" >> /etc/postfix/mysql-virtual-mailbox-maps.cf
 systemctl restart postfix
 postmap -q $user@$domain mysql:/etc/postfix/mysql-virtual-mailbox-maps.cf
 
 echo "smtp_generic_maps = hash:/etc/postfix/generic" >> /etc/postfix/main.cf
 
+touch /etc/postfix/generic
+postmap /etc/postfix/generic
+
+systemctl restart postfix
+
 #Demander le nom de la machine et remplir le fichier generic 
+
+echo "---------- Installation Dovecot ----------"
+
+sed -i -r '30s#mail_location = mbox:~/mail:INBOX=/var/mail/%u#mail_location = maildir:/var/mail/vhosts/%d/%n#g' /etc/dovecot/conf.d/10-mail.conf
+sed -i -r '114s/#mail_privileged_group =/mail_privileged_group = mail/g' /etc/dovecot/conf.d/10-mail.conf
+sed -i -r '10s/#disable_plaintext_auth = yes/disable_plaintext_auth = yes/g' /etc/dovecot/conf.d/10-auth.conf
+sed -i -r '122s/!include auth-system.conf.ext/#!include auth-system.conf.ext/g' /etc/dovecot/conf.d/10-auth.conf
+sed -i -r '123s/#!include auth-sql.conf.ext/!include auth-sql.conf.ext/g' /etc/dovecot/conf.d/10-auth.conf
+sed -i -r '20s/ driver = sql/ driver = static/g' /etc/dovecot/conf.d/auth-sql.conf.ext
+sed -i -r '21s# args = /etc/dovecot/dovecot-sql.conf.ext# args = uid=vhosts gid=vhosts home=/var/mail/vhosts/%d/%n#g' /etc/dovecot/conf.d/auth-sql.conf.ext
+sed -i -r '32s/#driver =/driver = sql/g' /etc/dovecot/dovecot-sql.conf.ext
+sed -i -r '71s/#connect =/connect = host=127.0.0.1 dbname=messagerie user=messagerieUser password=openclassrooms/g' /etc/dovecot/dovecot-sql.conf.ext
+sed -i -r '78s/#default_pass_scheme = MD5/default_pass_scheme = SHA512-CRYPT/g' /etc/dovecot/dovecot-sql.conf.ext
+sed -i -r "107s/password_query = \ /password_query = SELECT email as user, password FROM virtual_users WHERE email='%u'/g" /etc/dovecot/dovecot-sql.conf.ext
+
+systemctl restart dovecot
 
 fi
 
