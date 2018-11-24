8b5cd0ef1c7ef7d761274edef3919282934447ea
root <guillaume.cyril.zisa@gmail.com>
2018-11-24 22:28:49 +0100

    update clean

--- view/contact.php
+++ view/contact.php
@@ -1,21 +1,18 @@
-   <main role="main">
-
-       <center><div class="container">
-          <br><center><h3><strong>Contacts</strong></h3></center>
-           <a href="../controller/redirection.php?enter=tools" class="btn btn-success my-2">Boite a outils</a>
-           <a href="../controller/redirection.php?enter=servers" class="btn btn-primary my-2">Gestion de serveur(s) Debian 9</a>
-         </p>
-       </div></center>
-       <section class="jumbotron ">
-          <center><h3>Contact</h3>
-          Pour nous contacter , vous pouvez nous envoyer un mail a l'une des addresses suivantes :<br>
-          <A HREF="mailto:rcuvillier@intechinfo.fr">rcuvillier@intechinfo.fr</A><br>
-          <A HREF="mailto:nguengueogoula@intechinfo.fr">nguengueogoula@intechinfo.fr</A><br>
-          <A HREF="mailto:jprigent@intechinfo.fr">jprigent@intechinfo.fr</A><br>
-          <A HREF="mailto:fumey-humbert@intechinfo.fr">fumey-humbert@intechinfo.fr</A><br>
-          <A HREF="mailto:zisa@intechinfo.fr">zisa@intechinfo.fr</A><br>
-          </center>
-       </section>
-   </main>
- </body>
+<main role="main"><center>
+  <div class="container"><br>
+    <h3><strong>Contacts</strong></h3>
+    <a href="../controller/redirection.php?enter=tools" class="btn btn-success my-2">Boite a outils</a>
+    <a href="../controller/redirection.php?enter=servers" class="btn btn-primary my-2">Gestion de serveur(s) Debian 9</a>
+  </div>
+  <section class="jumbotron ">
+    <h3>Contact</h3>
+    Pour nous contacter , vous pouvez nous envoyer un mail a l'une des addresses suivantes :<br>
+    <a href="mailto:rcuvillier@intechinfo.fr">rcuvillier@intechinfo.fr</a><br>
+    <a href="mailto:nguengueogoula@intechinfo.fr">nguengueogoula@intechinfo.fr</a><br>
+    <a href="mailto:jprigent@intechinfo.fr">jprigent@intechinfo.fr</a><br>
+    <a href="mailto:fumey-humbert@intechinfo.fr">fumey-humbert@intechinfo.fr</a><br>
+    <a href="mailto:zisa@intechinfo.fr">zisa@intechinfo.fr</a><br>
+  </section></center>
+</main>
+</body>
 </html>
