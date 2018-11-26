568c096067fdf7262c380d4b57af86336926147b
Joran-FreaZy <38242052+Joran-FreaZy@users.noreply.github.com>
2018-11-26 09:45:58 +0100

    Ajout de l'image warning

--- view/server_mail.php
+++ view/server_mail.php
@@ -14,12 +14,13 @@
 
           </div>
           <div class="col-sm-10">
-            <div class="text-light bg-info" style="width:100%;border:solid black 1.5px;border-radius:4px;">
-              <br><h1>Prérequis : </h1>
+            <div class="text-light bg-info" style="width:100%;border:solid black 1.5px;border-radius:4px; margin: 30px auto;">
+              <br><h1>Prérequis : </h1> <h5>
 			  <br>	- Avoir les ports 25 et 143 d'ouverts sur sa box.
 			  <br>	- Avoir une machine avec une IP FIXE disposé à héberger le service.
 			  <br>	- Être administrateur du système.
-			  <br>	- Avoir un enregistrement MX pointant sur la machine hôte du serveur.
+			  <br>	- Avoir un enregistrement DNS MX pointant sur la machine hôte du serveur.
+			  <br>	- Avoir la liste de ses utilisateurs du service de messagerie. </h5>
             </div>
             </div>
           </div>
