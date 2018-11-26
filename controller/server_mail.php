5943da06fe58726a2dc1d3be8c0f6e738b0c5ffb
Joran-FreaZy <38242052+Joran-FreaZy@users.noreply.github.com>
2018-11-26 09:30:46 +0100

    Ajout du fichier controller pour le server mail

---
+++ controller/server_mail.php
@@ -0,0 +1,77 @@
+<main role="main"><center>
+  <div class="container"><br>
+  <h3><strong>Ajout de d'utilisateur(s) a des groupe(s)</strong></h3>
+    <a href="../controller/redirection.php?enter=tools" class="btn btn-success my-2">Boite a outils</a>
+    <a href="../controller/redirection.php?enter=servers" class="btn btn-primary my-2">Gestion de serveur(s) Debian 9</a>
+  </div>
+  <section class="jumbotron ">
+    <div class="ml-2">
+<?php
+#-------------------------------------------------------------------------------
+# GENERATION DU SCRIPT D'AJOUT DE DROITS AU GROUPE
+#-------------------------------------------------------------------------------
+
+#GÉNÉRATION DES VARIABLE DE FICHIERS--------------------------------------------
+$file_path="../script/script_client/add_user_".session_id().".sh";
+$file_name="add_user.sh";
+#VÉRIFICATION DE L'OPTION D'AUTO-DESTRUCTION------------------------------------
+if (isset( $_GET["auto_destruction"] )){ $rm = "rm ".$file_name; } else { $rm = ""; }
+#AJOUT DU LIEN DE TÉLÉCHARGEMENT & GUIDE----------------------------------------
+echo "<center><a class='btn btn-dark' href='".$file_path."'download='".$file_name."' target='_blank'>Télécharger le script </a></center><br>";
+include("../view/guide_execution.php");
+
+#-------------------------------------------------------------------------------
+# GÉNÉRATION DU SCRIPT
+#-------------------------------------------------------------------------------
+  if(isset($_GET['action']) && isset($_GET['under_action'])){
+    if(isset($_GET['username']) && isset($_GET['password'])){
+      $nb = count($_GET['username']);
+
+      #CONCATENATION DE TABLEAUX BASH---------------------------------------------
+      for( $i=0 ;$i<$nb ;$i++){
+        if ($i === 0 ){
+          $username="user[$i]=".$_GET['username'][$i]."\n";
+          $password="pass[$i]=".$_GET['password'][$i]."\n";
+        } else {
+          $username=$username."user[$i]=".$_GET['username'][$i]."\n";
+          $password=$password."pass[$i]=".$_GET['password'][$i]."\n";
+        }
+      }
+
+      #CRÉATION DE VARIABLES IMPORTANTES POUR ISOLER PHP & BASH-----------------
+      $user = '${user[$y]}';
+      $pass = '${pass[$y]}';
+
+      #GÉNÉRATION DU SCRIPT-----------------------------------------------------
+      $firstline = "
+      #!/bin/bash
+      #-------------------------------------------------------------------------
+      #SCRIPT D'AJOUT D'UTILISATEUR généré par IpSpawn.com
+      #-------------------------------------------------------------------------\n";
+
+      $script="
+        for ((y=0;y<".$nb.";y++))
+        do
+          #VERIFICATION DE L'EXISTANCE DE L'UTILISATEUR-------------------------
+          id -u ".$user."> /dev/null 2>&1
+          if [ $? == 0 ];
+          then
+            echo Nom d\'utilisateur déjà utilisé.
+          else
+            #CRÉATION D'UN UTILISATEUR------------------------------------------
+            useradd -m -d /home/".$user." -s /bin/bash ".$user."
+            echo ".$user.":".$pass."| chpasswd
+            groupdel -f  ".$user."
+          fi
+        done\n";
+      #RASSEMBLEMENT DES VARIABLES & CREATION DU SCRIPT-------------------------
+      $new_script = $firstline . $username . $password . $script . $rm;
+      $file = fopen($file_path, 'w+');
+      fputs($file,$new_script);
+
+    }
+  }
+?>
+
+</section>
+</main>
--- view/server_mail.php
+++ view/server_mail.php
@@ -15,9 +15,11 @@
           </div>
           <div class="col-sm-10">
             <div class="text-light bg-info" style="width:100%;border:solid black 1.5px;border-radius:4px;">
-              <br>Prérequis : <br><br>
-			  <br>	- Avoir une machine avec une IP FIXE disposé à héberger le service.<br>
-			  <br>	- 
+              <br><h1>Prérequis : </h1>
+			  <br>	- Avoir les ports 25 et 143 d'ouverts sur sa box.
+			  <br>	- Avoir une machine avec une IP FIXE disposé à héberger le service.
+			  <br>	- Être administrateur du système.
+			  <br>	- Avoir un enregistrement MX pointant sur la machine hôte du serveur.
             </div>
             </div>
           </div>
