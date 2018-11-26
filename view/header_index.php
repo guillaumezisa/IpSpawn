7902df1dc21f8b0afe93aa536ae3d93a77ab7a6b
Guillaume Zisa <guillaume.cyril.zisa@gmail.com>
2018-11-26 19:44:02 +0100

    Delete SomeBugs

--- view/header_index.php
+++ view/header_index.php
@@ -14,9 +14,9 @@
      <div class="navbar navbar-dark bg-dark shadow-sm">
        <div class="container">
          <div class="row">
-           <div class="col-3"> <a href='index.php'><img src='../style/images/Logo_white.png' width='75%'></a></div>
+           <div class="col-3"> <a href='index.php'><img src='style/images/Logo_white.png' width='75%'></a></div>
            <div class="col-7"></div>
-           <div class="col-2 mt-2 text-light"><a href='../controller/redirection.php?enter=contact' style='color:white; text-decoration:none'><img src='../style/images/contact.svg' width='20%'><strong><font size="4"> &#x200b Contacts</font></strong></div>
+           <div class="col-2 mt-2 text-light"><a href='controller/redirection.php?enter=contact' style='color:white; text-decoration:none'><img src='style/images/contact.svg' width='20%'><strong><font size="4"> &#x200b Contacts</font></strong></div>
          </div>
        </div>
      </div>
