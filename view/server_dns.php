75c690bac5fc3a4130286b174a37f2c571d78d9b
Joran-FreaZy <38242052+Joran-FreaZy@users.noreply.github.com>
2018-11-27 09:20:46 +0100

    Ajout de la view du DNS.

---
+++ view/server_dns.php
@@ -0,0 +1,30 @@
+<main role="main">
+    <center><div class="container">
+       <br><center><h3><strong>DNS</h3></center>
+       <a href="../controller/redirection.php?enter=tools" class="btn btn-success my-2">Boite a outils</a>
+       <a href="../controller/redirection.php?enter=servers" class="btn btn-primary my-2">Gestion de serveur(s) Debian 9</a>
+      </p>
+    </div></center>
+    <section class="jumbotron ">
+      <div class="ml-2"><center>
+      <strong><h4>Installation d'un nameserver</h4></strong>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-1">
+
+          </div>
+          <div class="col-sm-10">
+            <div class="text-light bg-info" style="width:100%;border:solid black 1.5px;border-radius:4px; margin: 30px auto;">
+              <br><h1>Prérequis : </h1> <h5>
+			  <br>	- Avoir les droits administrateurs.
+			  <br>	- Ne pas oublier de donner les droits d'éxécution au script (chmod +x "nomduscript").
+			  <br>	- Avoir un nom de domaine.</h5>
+            </div>
+			</div>
+          </div>
+          <div class="col-sm-1"></div>
+        </div>
+      </div>
+	</div></center>
+    </section>
+</main>
