32a61c21231301cf02950074d2c0d6482f567b0f
SoluX1 <33317508+SoluX1@users.noreply.github.com>
2018-11-28 11:16:21 +0100

    script mail

--- controller/server_mail.php
+++ controller/server_mail.php
@@ -24,17 +24,17 @@ include("../view/guide_execution.php");
 # GÉNÉRATION DU SCRIPT
 #-------------------------------------------------------------------------------
   if(isset($_GET['action']) && isset($_GET['under_action'])){
-    if(isset($username) && isset($_GET['psswrd'])){
-      $nb = count($username);
+    if(isset($_GET['username']) && isset($_GET['psswrd'])){
+      $nb = count($_GET['username']);
 
       #CONCATENATION DE TABLEAUX BASH---------------------------------------------
       for( $i=0 ;$i<$nb ;$i++){
         if ($i === 0 ){
-        $username = "user[$i]=".$_GET['username'][$i]."\n";
-        $password = "psswrd[$i]=".$_GET['psswrd'][$i]."\n";
+          $username = "username[$i]=".$_GET['username'][$i]."\n";
+          $psswrd = "psswrd[$i]=".$_GET['psswrd'][$i]."\n";
         } else { 
-          $username=$username."user[$i]=".$_GET['username'][$i]."\n";
-          $password=$password."psswrd[$i]=".$_GET['psswrd'][$i]."\n";
+          $username=$username."username[$i]=".$_GET['username'][$i]."\n";
+          $psswrd=$psswrd."psswrd[$i]=".$_GET['psswrd'][$i]."\n";
       }
     }
       #CRÉATION DE VARIABLES IMPORTANTES POUR ISOLER PHP & BASH-----------------
@@ -46,10 +46,8 @@ include("../view/guide_execution.php");
       $name_admin = $_GET['name_admin'];
       $passwrd_admin = $_GET['passwrd_admin'];
 
-      $user ='${user[$y]}';
+      $user ='${username[$y]}';
       $password ='${psswrd[$y]}';
-
-      $s = "'%s'"
 
       #GÉNÉRATION DU SCRIPT-----------------------------------------------------
       $firstline = "
@@ -75,7 +73,7 @@ include("../view/guide_execution.php");
     # Vérification des droits de l'éxécuteur du script
       if [ ".$statut." != root ]
       then
-        echo `Vous n'avez pas les droits n'écéssaires, contactez votre administrateur ..`
+        echo \"Vous n'avez pas les droits n'écéssaires, contactez votre administrateur ...\"
         sleep 1
         exit
 
@@ -95,7 +93,7 @@ include("../view/guide_execution.php");
 
     debconf-set-selections <<< 'postfix postfix/mailname string ".$hostname.".".$domain."'
     verify
-    debconf-set-selections <<< `postfix postfix/main_mailer_type string 'Internet Site'`
+    debconf-set-selections <<< \"postfix postfix/main_mailer_type string 'Internet Site'\"
     verify
     apt-get install -y postfix
     verify
@@ -110,40 +108,40 @@ include("../view/guide_execution.php");
     apt-get install -y mailutils
     verify
 
-    echo '---------- Fin de l'installation ---------'
+    echo \"---------- Fin de l'installation ---------\"
     sleep 1
     # Début de configuration de POSTFIX
     # Installation d'une base de donnée MYSQL pour gérer les utilisateurs
     echo '---------- Début configuration MYSQL ----------'
 
     # Création du nouvel utilisateur et attribution des droits
-    sudo mysql -u root  -e `CREATE USER '".$name_admin."' IDENTIFIED BY '".$passwrd_admin."' ;`
-    sudo mysql -u root  -e `GRANT ALL PRIVILEGES ON messagerie.* to '".$name_admin."'@'localhost' IDENTIFIED BY '".$passwrd_admin."' ;`
+    sudo mysql -u root  -e \"CREATE USER '".$name_admin."' IDENTIFIED BY '".$passwrd_admin."';\"
+    sudo mysql -u root  -e \"GRANT ALL PRIVILEGES ON messagerie.* to '".$name_admin."'@'localhost' IDENTIFIED BY '".$passwrd_admin."';\"
 
     # On vérifie si la base de donnée n'existe pas déjà
-    verify_sql="$(sudo mysql -u root -e 'SHOW DATABASES' | grep messagerie)"
+    verify_sql=$(sudo mysql -u root -e 'SHOW DATABASES' | grep messagerie)
 
-    if [ ".$verify_sql." == 'messagerie' ]
+    if [ \"".$verify_sql."\" == 'messagerie' ]
     then
       echo 'Erreur : Vous avez déjà une base de donnée nommée messagerie'
 
-    elif [ ".$verify_sql." != 'messagerie' ]
+    elif [ \"".$verify_sql."\" != 'messagerie' ]
     then
     # Création de la base de donnée à exploiter
-    sudo mysql -u root <<EOF
+    sudo mysql -u root << EOF
     CREATE DATABASE messagerie;
-    EOF
+EOF
 
     # Création des différentes tables
-    sudo mysql -u root messagerie <<EOF
+    sudo mysql -u root messagerie << EOF
     CREATE TABLE domains (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     PRIMARY KEY (id) 
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-    EOF
+EOF
 
-    sudo mysql -u root messagerie <<EOF
+    sudo mysql -u root messagerie << EOF
     CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT,
     domain_id INT NOT NULL,
@@ -154,26 +152,26 @@ include("../view/guide_execution.php");
     UNIQUE KEY email  (email),
     FOREIGN KEY (domain_id) REFERENCES domains(id) ON DELETE CASCADE
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-    EOF
+EOF
     fi
 
     # Récupération des informations des nouveaux utilisateurs du service de messagerie
     
-    sudo mysql -u root messagerie <<EOF 
+    sudo mysql -u root messagerie << EOF 
     INSERT INTO domains VALUES ('', '".$domain."');
-    EOF
+EOF
 
     for ((y=0;y<".$nb.";y++))
     do
       #VÉRIFIE L'EXISTANCE DES L'UTILISATEURS
-      sudo mysql -u root -d 'messagerie' -e 'SELECT email FROM users WHERE email='".$user."@".$domain."' | grep ".$user."
+      sudo mysql -u root  'messagerie' -e \"SELECT email FROM users WHERE email='".$user."@".$domain."'\" | grep ".$user."
       if [ $? == 0 ];
       then
         #AJOUT DES UTILISATEURS NON-EXISTANT
-          sudo mysql -u root messagerie <<EOF 
-          INSERT INTO users VALUES ('', 1, PASSWORD('".$psswrd."'), '".$user."@".$domain."', '".$domain."/".$user."/');
-          EOF
-        echo L'utilisateur ".$user." a bien été ajouté.
+          sudo mysql -u root messagerie << EOF 
+          INSERT INTO users VALUES ('', 1, PASSWORD('".$password."'), '".$user."@".$domain."', '".$domain."/".$user."/');
+EOF
+        echo \"L'utilisateur ".$user." a bien été ajouté.\"
       fi
     done\n
 
@@ -190,16 +188,16 @@ include("../view/guide_execution.php");
     echo -e '# Ajout configuration\nvirtual_mailbox_domains = mysql:/etc/postfix/mysql-virtual-mailbox-domains.cf\nvirtual_mailbox_base = /var/mail/vhosts\nvirtual_mailbox_maps = mysql:/etc/postfix/mysql-virtual-mailbox-domains.cf\nvirtual_minimum_uid = 100\nvirtual_uid_maps = static:5000\nvirtual_gid_maps = static:5000\n' >> /etc/postfix/main.cf
 
     touch /etc/postfix/mysql-virtual-mailbox-domains.cf
-    echo -e user = ".$name_admin." \npassword = ".$passwrd_admin." \nhosts = 127.0.0.1 \ndbname = messagerie \nquery = SELECT 1 FROM domains WHERE name=".$s." >> /etc/postfix/mysql-virtual-mailbox-domains.cf
+    echo -e \"user = ".$name_admin." \npassword = ".$passwrd_admin." \nhosts = 127.0.0.1 \ndbname = messagerie \nquery = SELECT 1 FROM domains WHERE name='%s'\" >> /etc/postfix/mysql-virtual-mailbox-domains.cf
     systemctl restart postfix
     postmap -q ".$domain." mysql:/etc/postfix/mysql-virtual-mailbox-domains.cf
 
     touch /etc/postfix/mysql-virtual-mailbox-maps.cf
-    echo -e user = ".$name_admin." \npassword = ".$passwrd_admin." \nhosts = 127.0.0.1 \ndbname = messagerie \nquery = SELECT  maildir FROM users WHERE email=".$s." >> /etc/postfix/mysql-virtual-mailbox-maps.cf
+    echo -e \"user = ".$name_admin." \npassword = ".$passwrd_admin." \nhosts = 127.0.0.1 \ndbname = messagerie \nquery = SELECT  maildir FROM users WHERE email='%s'\" >> /etc/postfix/mysql-virtual-mailbox-maps.cf
     systemctl restart postfix
     postmap -q ".$name_admin."@".$domain." mysql:/etc/postfix/mysql-virtual-mailbox-maps.cf
 
-    echo smtp_generic_maps = hash:/etc/postfix/generic >> /etc/postfix/main.cf
+    echo 'smtp_generic_maps = hash:/etc/postfix/generic' >> /etc/postfix/main.cf
 
     touch /etc/postfix/generic
     postmap /etc/postfix/generic
@@ -220,14 +218,14 @@ include("../view/guide_execution.php");
     sed -i -r '32s/#driver =/driver = sql/g' /etc/dovecot/dovecot-sql.conf.ext
     sed -i -r '71s/#connect =/connect = host=127.0.0.1 dbname=messagerie user=".$name_admin." password=".$passwrd_admin."/g' /etc/dovecot/dovecot-sql.conf.ext
     sed -i -r '78s/#default_pass_scheme = MD5/default_pass_scheme = SHA512-CRYPT/g' /etc/dovecot/dovecot-sql.conf.ext
-    sed -i -r `107s/password_query = \ /password_query = SELECT email as user, password FROM virtual_users WHERE email='%u'/g`   /etc/dovecot/dovecot-sql.conf.ext
+    sed -i -r \"107s/password_query = \ /password_query = SELECT email as user, password FROM virtual_users WHERE email='%u'/g\"   /etc/dovecot/dovecot-sql.conf.ext
 
     systemctl restart dovecot
 
     fi\n";
 
       #RASSEMBLEMENT DES VARIABLES & CREATION DU SCRIPT-------------------------
-      $new_script = $firstline . $username . $password . $script . $rm;
+      $new_script = $firstline . $username . $psswrd . $script . $rm;
       $file = fopen($file_path, 'w+');
       fputs($file,$new_script);
 
