1867e7c4bf5f91049ee5d958494cccf8b8d4fd92
SoluX1 <33317508+SoluX1@users.noreply.github.com>
2018-12-06 16:55:58 +0100

    script nginx

--- controller/server_web_nginx_gen.php
+++ controller/server_web_nginx_gen.php
@@ -43,10 +43,11 @@ if(isset($_GET['action']) && isset($_GET['under_action'])){
     apt install php -y
     apt install mariadb-server -y
 
-    sed -i -r 's/.*index index.html index.htm index.nginx-debian.html;*/index index.html index.htm index.php index.nginx-debian.html;/g' /etc/nginx/sites-available/default
-    sed -i -r 's/.*#location ~ \.php$ {*/location ~ \.php$ {/g' /etc/nginx/sites-available/default
-    sed -i -r 's/.*#include snippets/fastcgi-php.conf;*/include snippets/fastcgi-php.conf;/g' /etc/nginx/sites-available/default
-    sed -i -r 's/.*#fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;*/fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;}/g' /etc/nginx/sites-available/default
+    sed -i -r '44s/index.html/index.html index.php/g' /etc/nginx/sites-available/default
+    sed -i -r '56s/#//g' /etc/nginx/sites-available/default
+    sed -i -r '57s/#//g' /etc/nginx/sites-available/default
+    sed -i -r '60s/#//g' /etc/nginx/sites-available/default
+    sed -i -r '63s/#//g' /etc/nginx/sites-available/default
 
     ";
 
