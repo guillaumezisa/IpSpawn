c010af7a84376f591efb825226e1caf802b541e1
henri/bxt <henrifhumbert@gmail.com>
2018-12-10 10:24:32 +0100

    Guide tool box

---
+++ view/guide_execution_ip_range.php
@@ -0,0 +1,16 @@
+<?php
+echo"
+<div class='container' style='margin-top:-4%'>
+  <div class='row'>
+    <div class='col-sm-1'></div>
+    <div class='col-sm-10'>
+      <div class='text-light bg-info' style='width:100%;border:solid black 1.5px;border-radius:4px; margin: 30px auto;'>
+        <br><h4>Prérequis : </h4>
+        - Vous devez absolument remplir les deux champs.<br><br>
+      </div>
+    </div>
+  </div>
+<div class='col-sm-1'></div>
+</div>
+</div>";
+?>
