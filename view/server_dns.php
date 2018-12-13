64c768167c9a632ee13be374abfd9ef676f7ec4c
Joran-FreaZy <38242052+Joran-FreaZy@users.noreply.github.com>
2018-12-13 11:07:37 +0100

    test css

--- view/server_dns.php
+++ view/server_dns.php
@@ -33,11 +33,12 @@
 	<form action="../controller/redirection.php" method="GET">
 		<input type="hidden" name="action" value="dns">
 		<input type="hidden" name="under_action" value="install_dns_gen">
-		<h4>Configuration de la machine master </h4>
+		<h4><strong>Configuration de la machine master</strong></h4>
+		<br>
 		<label for="domain">Nom du domaine :</label><input type="text" name="domain" maxlength="50" required /><br/><br>
 		<label for="master">Nom de la machine master :</label><input type="text" name="master" maxlength="50" required /><br/><br>
-	<label for="private_ip">Ip privée de la machine master :</label><input type="text" name="private_ip" maxlength="50" required /><br/>
-        <h4>Configuration de vos zones </h4>
+	<label for="private_ip">Ip privée de la machine master :</label><input type="text" name="private_ip" maxlength="50" required /><br/><br>
+        <h4><strong>Configuration de vos zones</strong></h4>
         <button class="btn btn-dark" onclick="append(event)" id="new"<button>Ajouter une machine</button>
         <button class="btn btn-dark" onclick="reload(event)">Réinitialiser</button>
         <button class="btn btn-dark" onclick="rm_last_div(event)">Supprimer la dernière machine</button><br><br>
