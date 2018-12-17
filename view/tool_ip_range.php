59d413ab054d4963a8d779f4191ba10d982bb236
Joran-FreaZy <38242052+Joran-FreaZy@users.noreply.github.com>
2018-12-17 10:16:39 +0100

    Changements dans la vue toolbox

--- view/tool_ip_range.php
+++ view/tool_ip_range.php
@@ -9,31 +9,14 @@
         <?php
             include("../view/guide_execution_ip_range.php");
         ?>
-      <p align="center">
-          <font size="4" color="#850606">
-              <b>
-                  Input
-              </b>
-          </font>
-      </p>
-      <table border="0" width="100%" id="table1">
-          <form name="" action="../controller/redirection.php" method="GET">
-          <input type="hidden" name="action" value="ip_range">
-          <input type="hidden" name="under_action" value="result">
-          <tr>
-              <td width="50%" align="right">Adresse IP :</td>
-              <td><input type="text" name="ip" id="ip" maxlength="50"></td>
-          </tr>
-          <tr>
-              <td width="50%" align="right">Masque de sous réseau :</td>
-              <td><input type="text" name="mask" id="mask"maxlength="50"></td>
-              <td><input type="hidden" name="email" value="" /></td>
-          </tr>
-          <tr>
-              <td width="50%" align="right"><input type="submit" class="btn btn-dark" value="Submit"></td>
-          </tr>
-          </form>
-      </table>
-    </div></center>
+    <form name="" action="../controller/redirection.php" method="GET">
+    <input type="hidden" name="action" value="ip_range">
+    <input type="hidden" name="under_action" value="result">
+	<label for="ip"><strong>Adresse Ip : :</strong></label><input type="text" id="ip" name="ip" maxlength="50" required value=""/><br/><br>
+	<label for="ip"><strong>Masque de sous réseau :</strong></label><input type="text" id="mask" name="mask" maxlength="50" required value=""/><br/><br>
+	<input type="hidden" name="email" value="" />
+	<button type="submit" class="btn btn-dark">Valider</button></center><br><br>
+    </form>
+    </div>
   </section>
 </main>
