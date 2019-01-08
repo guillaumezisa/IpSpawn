a6fd717f98be410cb8c887eebc66d1b4d282ae1b
SoluX1 <33317508+SoluX1@users.noreply.github.com>
2019-01-08 11:27:45 +0100

    Gifs

---
+++ controller/easter_egg.php
@@ -0,0 +1,15 @@
+<?php
+include('../view/header.php');
+?>
+<center>
+<img src='../style/images/cat.gif'>
+<img src='../style/images/css.gif'>
+<img src='../style/images/giphy.gif'>
+<img src='../style/images/hacker.gif'>
+<img src='../style/images/kid.gif'>
+<img src='../style/images/egg.gif'>
+<img src='../style/images/pat.gif'>
+<img src='../style/images/rage.gif'>
+<img src='../style/images/y.gif'>
+<img src='../style/images/dickbutt'>
+</center>
