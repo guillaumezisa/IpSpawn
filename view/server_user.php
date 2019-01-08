4e1880889b601c5b8c5da7668ea0bb60d0c06622
Joran-FreaZy <38242052+Joran-FreaZy@users.noreply.github.com>
2019-01-08 10:08:58 +0100

    Correction titre utilisateurs

--- view/server_user.php
+++ view/server_user.php
@@ -1,7 +1,7 @@
 
 <main role="main">
     <center><div class="container">
-       <br><center><h3><strong>Gestion de Serveur(s)</strong></h3></center>
+       <br><center><h3><strong>Gestion des Utilisateurs</strong></h3></center>
        <a href="../controller/redirection.php?enter=tools" class="btn btn-success my-2">Boite à outils</a>
        <a href="../controller/redirection.php?enter=servers" class="btn btn-primary my-2">Gestion de serveur(s) Debian 9</a>
       </p>
