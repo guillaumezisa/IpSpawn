45461cfb7eaeab6dc4b65984cf59f6ea882fada6
Rodney <rodney.nguengue@gmail.com>
2019-01-09 11:30:59 +0100

    correction

--- view/guide_user.php
+++ view/guide_user.php
@@ -1,6 +1,6 @@
 <div class='container' style='margin-top:-4%'>
   <div class='text-light bg-info' style='width:100%;border:solid black 1.5px;border-radius:4px; margin: 30px auto; mar'><br>
-<h5 style='margin-left:10%'>Les utilisateurs peuvent vous permettre de travailler a plusieurs sur un serveur,<br>
-de créer plusieurs espaces personnelles<br></h5><br>
+<h5 style='margin-left:10%'>Les utilisateurs peuvent vous permettre de travailler à plusieurs sur un serveur,<br>
+de créer plusieurs espaces personnels<br></h5><br>
 </div>
 </div>
--- view/guide_web.php
+++ view/guide_web.php
@@ -1,7 +1,7 @@
 <div class='container' style='margin-top:-4%'>
   <div class='text-light bg-info' style='width:100%;border:solid black 1.5px;border-radius:4px; margin: 30px auto; mar'><br>
 <h5 style='margin-left:10%'>Un serveur web vous permettera de faire votre site internet,<br>
-pour le rendre publique vous devrez ouvrir le port 80(http) et/ou leport 443(https) sur votre routeur.<br>
-Pour eviter certain problème assez rare il vaut mieu rendre le serveur statique.</h5><br>
+pour le rendre publique vous devrez ouvrir le port 80(http) et/ou le port 443(https) sur votre routeur.<br>
+Pour eviter certain problème assez rare il vaut mieux rendre le serveur statique.</h5><br>
 </div>
 </div>
