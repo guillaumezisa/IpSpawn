5af2b5cf96206912c904b71e9cbc6ad35f67d7fd
Rodney <rodney.nguengue@gmail.com>
2019-01-09 11:08:14 +0100

    correction

--- view/guide_group_reset.php
+++ view/guide_group_reset.php
@@ -1,7 +1,7 @@
 <div class='container' style='margin-top:-4%'>
   <div class='text-light bg-info' style='width:100%;border:solid black 1.5px;border-radius:4px; margin: 30px auto; mar'><br>
     <h5 style='margin-left:10%'>
-      Supprimer un group vous permet de supprimer tous les liens entre les utilisateurs et ce groupe.<br>Si vous avez donner des privilèges spécifiques vous devréer réinitialiser le sudoers <br><br>
+      Supprimer un groupe vous permet de supprimer tous les liens entre les utilisateurs et ce groupe.<br>Si vous avez donner des privilèges spécifiques vous devrez réinitialiser le sudoers <br><br>
     </h5>
   </div>
 </div>
