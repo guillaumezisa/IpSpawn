63653098905ae872f2bd42e38adff8b17193a34f
Rodney <rodney.nguengue@gmail.com>
2019-01-09 11:09:36 +0100

    correction

--- view/guide_group.php
+++ view/guide_group.php
@@ -1,6 +1,6 @@
 <div class='container' style='margin-top:-4%'>
   <div class='text-light bg-info' style='width:100%;border:solid black 1.5px;border-radius:4px; margin: 30px auto; mar'><br>
 <h5 style='margin-left:10%'>Les groupes permettent de rassembler plusieurs utilisateurs sur un serveur,<br>
-de cette manière on pourra modifier les droits d'un groupe plutot que de gere un utilisateurs individuel.<br></h5><br>
+de cette manière on pourra modifier les droits d'un groupe plutot que de gérer un utilisateur individuel.<br></h5><br>
 </div>
 </div>
