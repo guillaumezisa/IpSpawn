f34ce062efcf8be1ef473a7d84ce4e63481ef5ed
guillaumezisa <guillaume.cyril.zisa@gmail.com>
2019-01-11 15:00:22 +0100

    Update Firefox style + guides + correction

---
+++ view/guide_group_add.php
@@ -0,0 +1,10 @@
+</center>
+<div class='container' style='margin-top:-4%'>
+  <div class='text-light bg-info' style='width:100%;border:solid black 1.5px;border-radius:4px; margin: 30px auto; mar'><br>
+    <h5 style='margin-left:10%'>
+      Ajouter un groupe est important pour quelques scripts tel que samba,<br>
+      pour acceder a une gestions des droits et des groupes il faudra également créer au préalable un groupe .<br><br>
+    </h5>
+  </div>
+</div>
+<center>
---
+++ view/guide_group_right.php
@@ -0,0 +1,9 @@
+</center>
+<div class='container' style='margin-top:-4%'>
+  <div class='text-light bg-info' style='width:100%;border:solid black 1.5px;border-radius:4px; margin: 30px auto; mar'><br>
+    <h5 style='margin-left:10%'>
+      Ici vous pourrez configurer vos groupes de manière a les rendre exploitables.
+    </h5>
+  </div>
+</div>
+<center>
