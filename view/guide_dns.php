e2731119a6198c904c267756d14f3a1c1540cd57
Rodney <rodney.nguengue@gmail.com>
2019-01-11 14:55:54 +0100

    Correction again again again

--- view/guide_dns.php
+++ view/guide_dns.php
@@ -3,7 +3,7 @@
     <h5 style='margin-left:10%'>
 	     Cette partie du site est consacrée à la mise en place d'un serveur DNS dans un réseau local,<br>
        Pour mettre en place votre serveur DNS veillez à avoir tous vos serveurs sur ip fixe,<br>
-       ainsi qu'un un nom de domaine. Votre serveur principal DNS sera votre DNS master,<br>
+       ainsi qu'un nom de domaine. Votre serveur principal DNS sera votre DNS master,<br>
        par la suite nous vous demanderons de créer vos zones :<br><br>
 
       <b style="color:red">NS</b> :Un enregistrement NS identifie les serveurs de noms de la zone hébergée.<br>
