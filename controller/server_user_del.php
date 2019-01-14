c892ba1e490d4a9eaeb97bb20848d28b949b51b3
guillaumezisa <guillaume.cyril.zisa@gmail.com>
2019-01-14 14:33:24 +0100

    Update

--- controller/server_user_del.php
+++ controller/server_user_del.php
@@ -42,8 +42,7 @@ if(isset($_GET['action']) && isset($_GET['under_action'])){
     $root='"root"';
 
     #GÉNÉRATION DU SCRIPT-------------------------------------------------------
-    $firstline = "
-#!/bin/bash
+    $firstline = "#!/bin/bash
 #-------------------------------------------------------------------------------
 #SCRIPT DE SUPPRESSION D'UTILISATEURS généré par IpSpawn.com
 #V.1.2
